4f74370ab6e66867ce49d6b46416b9debdf77814
himi <bxcx@163.com>
2016-09-14 16:37:25 +0800

    main

---
+++ app/src/main/java/ear/life/ui/MainTabActivity.kt
@@ -0,0 +1,30 @@
+package ear.life.ui
+
+import com.hm.library.base.BaseMainActivity
+import ear.life.R
+import kotlinx.android.synthetic.main.activity_main_tab.*
+
+class MainTabActivity(override var layoutResID: Int = R.layout.activity_main_tab) : BaseMainActivity() {
+
+    lateinit var titles: Array<String>
+
+    override fun setUIParams() {
+        titles = resources.getStringArray(R.array.bottom_bar_labels)
+        titles.forEach { mTabs.add(BlankFragment()) }
+    }
+
+    override fun initComplete() {
+        //三秒后执行
+        runDelayed({
+            //模拟红点
+            main_tabpage.setIndicateDisplay(3, true)
+        }, 3000)
+
+        //是否可以滑动，滑动是否有渐变效果
+        main_tabpage.setStyle(true, true)
+    }
+
+    override fun onTabSelected(index: Int) {
+        tv_title.text = titles[index]
+    }
+}
