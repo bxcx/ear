c21f853a11215cea4373c1bcab46c4eaa862cff2
himi <bxcx@163.com>
2017-03-19 23:37:31 +0800

    no message

--- app/src/main/java/ear/life/ui/mine/MineFragment.kt
+++ app/src/main/java/ear/life/ui/mine/MineFragment.kt
@@ -3,6 +3,7 @@ package ear.life.ui.mine
 
 import android.content.Intent
 import android.graphics.Color
+import android.net.Uri
 import android.view.View
 import com.alibaba.sdk.android.feedback.impl.FeedbackAPI
 import com.hm.library.base.BaseFragment
@@ -101,7 +102,11 @@ class MineFragment(override var layoutResID: Int = R.layout.fragment_mine) : Bas
         layout_setting.setOnClickListener(this)
         layout_message.setOnClickListener(this)
         layout_activity.setOnClickListener(this)
-        layout_join.setOnClickListener(this)
+        layout_join.onClick {
+            if (!joinQQGroup("RT20txYU4wfHtN8g-gb68ChnNu0kYWls")) {
+                showTips(TipsToast.TipType.Smile, "QQ群号：3318916")
+            }
+        }
 
         if (App.user == null) {
             iv_head.setImageResource(R.drawable.ic_launcher)
@@ -128,6 +133,29 @@ class MineFragment(override var layoutResID: Int = R.layout.fragment_mine) : Bas
         super.onActivityResult(requestCode, resultCode, data)
         loadData()
         su?.onActivityResult(requestCode, resultCode, data)
+    }
+
+    /****************
+
+     * 发起添加群流程。群号：耳朵纯音乐(3318916) 的 key 为： RT20txYU4wfHtN8g-gb68ChnNu0kYWls
+     * 调用 joinQQGroup(RT20txYU4wfHtN8g-gb68ChnNu0kYWls) 即可发起手Q客户端申请加群 耳朵纯音乐(3318916)
+
+     * @param key 由官网生成的key
+     * *
+     * @return 返回true表示呼起手Q成功，返回fals表示呼起失败
+     */
+    fun joinQQGroup(key: String): Boolean {
+        val intent = Intent()
+        intent.data = Uri.parse("mqqopensdkapi://bizAgent/qm/qr?url=http%3A%2F%2Fqm.qq.com%2Fcgi-bin%2Fqm%2Fqr%3Ffrom%3Dapp%26p%3Dandroid%26k%3D" + key)
+        // 此Flag可根据具体产品需要自定义，如设置，则在加群界面按返回，返回手Q主界面，不设置，按返回会返回到呼起产品界面    //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        try {
+            startActivity(intent)
+            return true
+        } catch (e: Exception) {
+            // 未安装手Q或安装的版本不支持
+            return false
+        }
+
     }
 
 }
